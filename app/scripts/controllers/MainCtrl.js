c1226d5aad9160de415a743ffcf712eb8c03ad49
mortendybdal <morten.dybdal@creuna.dk>
2014-09-02 13:57:35 +0200

    Added a timeout on the loading flow to finish the fade animation

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module('waitingRoomApp')
-   .controller('MainCtrl', function ($scope, $rootScope) {
+   .controller('MainCtrl', function ($scope, $rootScope, $timeout) {
         $rootScope.$on("event:load_start", function () {
             $scope.is_loading_content = true;
         });
 
         $rootScope.$on("event:load_stop", function () {
-            $scope.is_loading_content = false;
+            $timeout(function () {
+                $scope.is_loading_content = false;
+            }, 600);
         });
    });
